5a5dff1527dce50eaa93088f21faa24ce3500f2f
Zemistr <zemistr@gmail.com>
2016-04-14 21:00:45 +0200

    Fixed wrong piwik url

--- stream_cz.php
+++ stream_cz.php
@@ -24,4 +24,4 @@ $downloader->setLogger($logger);
 $downloader->setLoader($loader);
 $data = $downloader->send($url);
 
-@file_get_contents('http://piwik.zdft.net/piwik/piwik.php?idsite=7&rec=1&url=' . $url . '&action_name=' . rawurlencode($data['title']) . '&cip=' . @$_SERVER['REMOTE_ADDR'] . '&token_auth=13a8deb4bf6f0bbec63240301c0142c0');
+@file_get_contents('http://piwik.zdft.net/piwik.php?idsite=7&rec=1&url=' . $url . '&action_name=' . rawurlencode($data['title']) . '&cip=' . @$_SERVER['REMOTE_ADDR'] . '&token_auth=13a8deb4bf6f0bbec63240301c0142c0');
